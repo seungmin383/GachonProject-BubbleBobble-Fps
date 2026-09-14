6f8fefdf978ddca04728d9a5db30ae976fa7b35a
SEUNGMIN_BAEK <seungmin03ws@gmail.com>
2026-09-14 20:21:57 +0900

    [feat] : Bubble State 분리, 시간이 지나거나 몬스터를 잡으면 위로 올라감

--- Assets/01_Scripts/04_Gun/Bullet/Bubble.cs
+++ Assets/01_Scripts/04_Gun/Bullet/Bubble.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using Asset.Script.Monster;
-using Unity.VisualScripting;
 using Asset.Script.Interfaces;
 
 namespace Asset.Script.Weapon
@@ -10,17 +8,39 @@ namespace Asset.Script.Weapon
         [SerializeField]
         private float _speed = 10.0f;
         [SerializeField]
-        private float _lifeTime = 5.0f;
+        private float _floatingSpeed = 3.0f;
+        [SerializeField]
+        private float _lifeTime = 20.0f;
+
+        [SerializeField]
+        private float _timeToFloating = 3.0f;
 
         private float _elapsedTime;
         private bool _isBurst;
+        private BubbleState _currentState = BubbleState.Flying;
+        
+        [SerializeField]
+        private float _floatingTransitionTime = 1.0f;
+
+        private Vector3 _moveDirection;
+        private float _floatingElapsedTime;
 
         private ICapturable _capturable;
 
+        private enum BubbleState
+        {
+            Flying, Floating, Captured, Popping, Bursting
+        }
+
+        private void Start()
+        {
+            _moveDirection = transform.forward;
+        }
+
         void Update()
         {
-            Move();
-            UpdateLifeTime();
+            UpdateState();
+            AdaptState();
         }
 
         private void OnTriggerEnter(Collider other)
@@ -35,6 +55,7 @@ namespace Asset.Script.Weapon
                 if(captured.TryCapture(this))
                 {
                     _capturable = captured;
+                    _currentState = BubbleState.Captured;
                 }
             }
         }
@@ -43,27 +64,70 @@ namespace Asset.Script.Weapon
         {
             transform.position += transform.forward * _speed * Time.deltaTime;
         }
+        private void MoveFloating()
+        {
+            _floatingElapsedTime += Time.deltaTime;
 
-        private void UpdateLifeTime()
+            float t = _floatingElapsedTime / _floatingTransitionTime;
+            t = Mathf.Clamp01(t);
+
+            Vector3 direction = Vector3.Slerp(_moveDirection, Vector3.up, t);
+            transform.position += direction * _floatingSpeed * Time.deltaTime;
+        }
+
+        private void UpdateState()
         {
             _elapsedTime += Time.deltaTime;
+
+            if(_elapsedTime > _timeToFloating && _currentState == BubbleState.Flying)
+            {
+                _currentState = BubbleState.Floating;
+            }
 
             if (_elapsedTime > _lifeTime)
             {
                 Burst();
             }
         }
+
         public void Burst()
         {
             if (_isBurst)
                 return;
 
             _isBurst = true;
+            _currentState = BubbleState.Bursting;
 
+            // 만약 버블이 무언가를 잡고있다면
             _capturable?.OnBubbleBurst();
             _capturable = null;
 
             Destroy(gameObject);
+        }
+
+        private void AdaptState()
+        {
+            switch(_currentState)
+            {
+                case BubbleState.Flying:
+                    Move();
+                    break;
+
+                case BubbleState.Floating:
+                    MoveFloating();
+                    break;
+
+                case BubbleState.Captured:
+                    MoveFloating();
+                    break;
+
+                case BubbleState.Popping:
+                    break;
+
+                case BubbleState.Bursting:
+                    break;
+
+            }
         }
     }
 }
